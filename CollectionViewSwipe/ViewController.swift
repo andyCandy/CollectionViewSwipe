b8d2cd340afd2db9dedb8cafca4b596456291fe4
Alex Stratu <alexstratu@gmail.com>
2015-03-19 16:15:31 +0200

    added next & back buttons; added page control indicator

--- CollectionViewSwipe/ViewController.swift
+++ CollectionViewSwipe/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UIScrollViewDelegate {
+    
     
     let images = ["img1", "img2", "img3", "img4", "img5", "img6", "img7"]
     
     
     @IBOutlet weak var collectionView: UICollectionView!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    @IBAction func goToNextImage(sender: AnyObject) {
         
+        var indexPathArray = collectionView.indexPathsForVisibleItems()
+        var currentIndexPath = indexPathArray.first as NSIndexPath
+        var nextIndexPath = NSIndexPath(forItem: currentIndexPath.item + 1, inSection: 0)
 
+        // protecting against crash
+        
+        if nextIndexPath.item < images.count {
+            
+            self.collectionView.scrollToItemAtIndexPath(nextIndexPath, atScrollPosition: UICollectionViewScrollPosition.CenteredHorizontally, animated: true)
+            pageControl.currentPage = nextIndexPath.item
+        }
     }
+    
+    @IBAction func goBackToImage(sender: AnyObject) {
+        
+        var indexPathArray = collectionView.indexPathsForVisibleItems()
+        var currentIndexPath = indexPathArray.first as NSIndexPath
+        var previousIndexPath = NSIndexPath(forItem: currentIndexPath.item - 1, inSection: 0)
+        
+        // protecting against crash
+        
+        if previousIndexPath.item >= 0 {
+            
+            self.collectionView.scrollToItemAtIndexPath(previousIndexPath, atScrollPosition: UICollectionViewScrollPosition.CenteredHorizontally, animated: true)
+            pageControl.currentPage = previousIndexPath.item
+        }
+    }
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    
     
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
      
@@ -42,8 +70,34 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         var imageName = images[indexPath.row] as String
         cell.slideImage.image = UIImage(named: imageName)
         
+        
         return cell
     }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        pageControl.numberOfPages = images.count
+        pageControl.currentPage = 0
+    }
+    
+    
+    // MARK: UIScrollViewDelegate
+    
+    // updating page control's current page when scroll stops
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        
+        var indexPathArray = collectionView.indexPathsForVisibleItems()
+        var currentIndexPath = indexPathArray.first as NSIndexPath
+        
+        pageControl.currentPage = currentIndexPath.item
+    }
+    
 
 }
 
