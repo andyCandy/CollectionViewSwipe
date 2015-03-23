fb71fb3bd96da6489771bb6cf77b46c202e3c1e2
Alex Stratu <alexstratu@gmail.com>
2015-03-23 16:40:12 +0200

    custom segmented view added; color selector added

---
+++ CollectionViewSwipe/ColorSelectionCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ColorSelectionCollectionViewCell.swift
+//  CollectionViewSwipe
+//
+//  Created by Alex on 3/23/15.
+//  Copyright (c) 2015 WebFuture. All rights reserved.
+//
+
+import UIKit
+
+class ColorSelectionCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var colorSelectionImageView: UIImageView!
+    
+}
--- CollectionViewSwipe/ViewController.swift
+++ CollectionViewSwipe/ViewController.swift
@@ -8,20 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UIScrollViewDelegate {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UIScrollViewDelegate, SMSegmentViewDelegate {
     
     // MARK: Data
     
     let images = ["img1", "img2", "img3", "img4", "img5", "img6", "img7"]
+    let colors = ["black.jpeg", "coconut.jpeg", "flamingo pink.jpeg", "tropical orange.jpeg", "kalua nude.jpeg", "pacific blue.jpeg", "colibri green.jpeg"]
+    
+    var segmentView: SMSegmentView!
+    var margin: CGFloat = 30.0
+    
+    let mediumGray = UIColor(red: 153/255, green: 153/255, blue: 153/255, alpha: 1)
+
     
     
     // MARK: Outlets & Actions
     
-    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var swipeCV: UICollectionView!
+    
+    @IBOutlet weak var colorSelectionCV: UICollectionView!
+    
     
     @IBAction func goToNextImage(sender: AnyObject) {
         
-        var indexPathArray = collectionView.indexPathsForVisibleItems()
+        var indexPathArray = swipeCV.indexPathsForVisibleItems()
         var currentIndexPath = indexPathArray.first as NSIndexPath
         var nextIndexPath = NSIndexPath(forItem: currentIndexPath.item + 1, inSection: 0)
 
@@ -29,14 +39,14 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         
         if nextIndexPath.item < images.count {
             
-            self.collectionView.scrollToItemAtIndexPath(nextIndexPath, atScrollPosition: UICollectionViewScrollPosition.CenteredHorizontally, animated: true)
+            self.swipeCV.scrollToItemAtIndexPath(nextIndexPath, atScrollPosition: UICollectionViewScrollPosition.CenteredHorizontally, animated: true)
             pageControl.currentPage = nextIndexPath.item
         }
     }
     
     @IBAction func goBackToImage(sender: AnyObject) {
         
-        var indexPathArray = collectionView.indexPathsForVisibleItems()
+        var indexPathArray = swipeCV.indexPathsForVisibleItems()
         var currentIndexPath = indexPathArray.first as NSIndexPath
         var previousIndexPath = NSIndexPath(forItem: currentIndexPath.item - 1, inSection: 0)
         
@@ -44,7 +54,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         
         if previousIndexPath.item >= 0 {
             
-            self.collectionView.scrollToItemAtIndexPath(previousIndexPath, atScrollPosition: UICollectionViewScrollPosition.CenteredHorizontally, animated: true)
+            self.swipeCV.scrollToItemAtIndexPath(previousIndexPath, atScrollPosition: UICollectionViewScrollPosition.CenteredHorizontally, animated: true)
             pageControl.currentPage = previousIndexPath.item
         }
     }
@@ -53,7 +63,9 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     @IBOutlet weak var pageScoll: UIScrollView!
     
-    @IBOutlet weak var sizesSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var psvLabel: UILabel!
+    
+    
     
     
     
@@ -66,23 +78,50 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-        return images.count
+        if collectionView.tag == 1 {
+            
+            return images.count
+        }
+
+        if collectionView.tag == 2 {
+        
+            return colors.count
+        }
+            
+        return 0
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as MyCollectionViewCell
+        let cell = colorSelectionCV.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as ColorSelectionCollectionViewCell
         
-        // Configure the cell
+        if collectionView.tag == 1 {
+            
+            let swipeCell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as MyCollectionViewCell
+            
+            // Configure the cell
+            
+            // Image aspect fill & hidden horizontal scroll indicator set in storyboard
+            
+            var imageName = images[indexPath.row] as String
+            swipeCell.slideImage.image = UIImage(named: imageName)
+            
+            return swipeCell
+            
+        } else if collectionView.tag == 2 {
         
-        // Image aspect fill & hidden horizontal scroll indicator set in storyboard
+            let colorCell = colorSelectionCV.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as ColorSelectionCollectionViewCell
+            
+            // Configure the cell
+            
+            var colorName = colors[indexPath.row] as String
+            colorCell.colorSelectionImageView.image = UIImage(named: colorName)
+            
+            return colorCell
 
-        var imageName = images[indexPath.row] as String
-        cell.slideImage.image = UIImage(named: imageName)
+        }
         
-        
-    return cell
-        
+        return cell
     }
     
     
@@ -92,22 +131,50 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
         
-        var indexPathArray = collectionView.indexPathsForVisibleItems()
+        var indexPathArray = swipeCV.indexPathsForVisibleItems()
         var currentIndexPath = indexPathArray.first as NSIndexPath
         
         pageControl.currentPage = currentIndexPath.item
     }
+    
     
     // MARK: -
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Settig page scroll
+        
         pageScoll.delegate = self
         
         pageScoll.contentSize.width = UIScreen.mainScreen().bounds.width
         pageScoll.contentSize.height = 936
         
+        
+        // Create a custom SM segmented view
+        
+            let segmentViewProperties : Dictionary<String, AnyObject> = [   keySegmentTitleFont: UIFont.systemFontOfSize(14),
+                                                                            keySegmentOnSelectionColour: mediumGray,
+                                                                            keySegmentOffSelectionColour: UIColor.whiteColor(),
+                                                                            keyContentVerticalMargin: 10.0]
+            
+            segmentView = SMSegmentView(frame: CGRect(x: margin, y: 532, width: self.view.frame.size.width - margin*2, height: 30),
+                separatorColour: mediumGray, separatorWidth: 0.5, segmentProperties: segmentViewProperties)
+            
+            segmentView.delegate = self
+            
+            segmentView.addSegmentWithTitle("34", onSelectionImage: nil, offSelectionImage: nil)
+            segmentView.addSegmentWithTitle("36", onSelectionImage: nil, offSelectionImage: nil)
+            segmentView.addSegmentWithTitle("38", onSelectionImage: nil, offSelectionImage: nil)
+            segmentView.addSegmentWithTitle("40", onSelectionImage: nil, offSelectionImage: nil)
+            segmentView.addSegmentWithTitle("42", onSelectionImage: nil, offSelectionImage: nil)
+            segmentView.addSegmentWithTitle("44", onSelectionImage: nil, offSelectionImage: nil)
+            segmentView.addSegmentWithTitle("LSM", onSelectionImage: nil, offSelectionImage: nil)
+            
+            // You can programmatically select/deselect a segment by calling selectSegmentAtIndex(index: Int)
+            segmentView.selectSegmentAtIndex(0)
+            
+            self.pageScoll.addSubview(self.segmentView)
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -115,7 +182,22 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         pageControl.numberOfPages = images.count
         pageControl.currentPage = 0
         
-//        sizesSegmentedControl.setTitleTextAttributes([NSForegroundColorAttributeName : UIColor.redColor()], forState: UIControlState.Normal)
+        psvLabel.hidden = true
+        
+//        colorSelectionCV.scrollRectToVisible(rect: CGRect(origin: 0, 0, size: CGSize(UIScreen().bounds.width, 30)), animated: false)
+        
+        
+
+        
+    }
+    
+    // MARK: SMSegment Delegate
+    
+    func didSelectSegmentAtIndex(segmentIndex: Int) {
+        /*
+        Replace the following line to implement what you want the app to do after the segment gets tapped.
+        */
+        println("Select segment at index: \(segmentIndex)")
     }
     
 
